81103a36cbcd7049e0ca9cc25059d50b2daecff8
naoki_stepmaina <na2424@hotmail.co.jp>
2015-08-05 08:14:56 +0900

    マップを配置

--- ChatUni/Assets/Script/NameText.cs
+++ ChatUni/Assets/Script/NameText.cs
@@ -8,7 +8,7 @@ public class NameText : MonoBehaviour
 	public Camera mainCamera;
 
 	public RectTransform parentRectTrans;
-
+	[SerializeField] Transform myTransform;
 	[SerializeField] RectTransform thisRectTransform;
 	[SerializeField] Text nameText;
 
@@ -22,20 +22,28 @@ public class NameText : MonoBehaviour
 		nameText.text = name;
 	}
 	
-	void Update () 
+//	void Update () 
+//	{
+//		if (nameText.text.Length == 0) return;
+//		UpdateUiLocalPosFromTargetPos();
+//	}
+//
+//	/// <summary>
+//	/// キャラの上に名前を表示.
+//	/// </summary>
+//	void UpdateUiLocalPosFromTargetPos()
+//	{
+//		Vector3 screenPos = mainCamera.WorldToScreenPoint(playerTransform.position);
+//		Vector2 localPos = Vector2.zero;
+//		RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTrans, screenPos, mainCamera, out localPos);
+//		thisRectTransform.localPosition = localPos + offset;
+//	}
+
+	void LateUpdate()
 	{
 		if (nameText.text.Length == 0) return;
-		UpdateUiLocalPosFromTargetPos();
+		Vector3 screenPos = mainCamera.WorldToScreenPoint(playerTransform.position);
+		myTransform.position = new Vector3 (screenPos.x, screenPos.y, 0);
 	}
 
-	/// <summary>
-	/// キャラの上に名前を表示.
-	/// </summary>
-	void UpdateUiLocalPosFromTargetPos()
-	{
-		Vector3 screenPos = mainCamera.WorldToScreenPoint(playerTransform.position);
-		Vector2 localPos = Vector2.zero;
-		RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTrans, screenPos, mainCamera, out localPos);
-		thisRectTransform.localPosition = localPos + offset;
-	}
 }
